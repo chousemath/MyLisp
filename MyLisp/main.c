0b926e00d7cc3bcfefbcab91986a7ecc635af561
Joseph Sungpil Choi <chousemath@gmail.com>
2017-06-05 21:34:10 +0900

    first commit

---
+++ MyLisp/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  MyLisp
+//
+//  Created by Joseph Sungpil Choi on 6/5/17.
+//  Copyright © 2017 Joseph Sungpil Choi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Hello again world!\n");
+    return 0;
+}
